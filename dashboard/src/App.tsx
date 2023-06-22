25fc9e00a0af77afee07759a1a7b80ea76b867ca
Yash Jane <yashjane99@gmail.com>
2023-06-22 17:15:53 +0530

    feat: add login route

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -3,6 +3,7 @@ import { UserProvider } from './utils/auth/UserProvider'
 import { ProtectedRoute } from './utils/auth/ProtectedRoute'
 import { SignUp } from './pages/features/auth/SignUp'
 import { FrappeProvider } from 'frappe-react-sdk'
+import { LogIn } from './pages/features/auth/LogIn'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
             <Routes>
               {/** Public Routes */}
               <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/sign-in" element={<LogIn />} />
 
               {/** Private Routes */}
               <Route path="/" element={<ProtectedRoute />} />
